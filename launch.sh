3552b63102b91d0b5183ffcb567e8d821d06b134
Marek Novotný <hotmana76@gmail.com>
2017-03-20 22:44:21 +0100

    fixed paths in launch.sh

--- launch.sh
+++ launch.sh
@@ -7,9 +7,9 @@ CONFIG_FOLDER="$HOME/.config/syncthing"
 CONFIG_FILE="$CONFIG_FOLDER/config.xml"
 
 if [ ! -f "$CONFIG_FILE" ]; then
-    $HOME/syncthing-linux-arm-v0.14.24/syncthing -generate="$CONFIG_FOLDER"
+    /usr/local/bin/syncthing -generate="$CONFIG_FOLDER"
 fi
 
 chown -R syncthing:syncthing "$HOME"
 
-exec su - syncthing -c $HOME/syncthing-linux-arm-v0.14.24/syncthing
+exec su - syncthing -c /usr/local/bin/syncthing
